2bc12950e80a299d60d4a98cbb297d5b216b057f
yusupmyratGihub <ashgabatmekan@gmail.com>
2023-01-23 13:30:50 +0100

    login ile register pages ready

--- screens/login-screen.js
+++ screens/login-screen.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    // Perform login logic here, such as sending a request to an authentication server
+    if (!email || !password) {
+      setError('Please enter your email and password');
+    } else {
+      // Successful login logic here
+    }
+  };
+
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={{ borderBottomWidth: 1, padding: 10 }}
+      />
+      <Text>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry={true}
+        style={{ borderBottomWidth: 1, padding: 10 }}
+      />
+      {error && <Text>{error}</Text>}
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default LoginScreen;
-
-const styles = StyleSheet.create({});
--- screens/register-screen.js
+++ screens/register-screen.js
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 const RegisterScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleRegister = () => {
+    // Perform registration logic here, such as sending a request to a server
+    if (!name || !email || !password || !confirmPassword) {
+      setError('Please fill out all fields');
+    } else if (password !== confirmPassword) {
+      setError('Passwords do not match');
+    } else {
+      // Successful registration logic here
+    }
+  };
+
   return (
     <View>
-      <Text>RegisterScreen</Text>
+      <Text>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={text => setName(text)}
+        style={{ borderBottomWidth: 1, padding: 10 }}
+      />
+      <Text>Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={{ borderBottomWidth: 1, padding: 10 }}
+      />
+      <Text>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry={true}
+        style={{ borderBottomWidth: 1, padding: 10 }}
+      />
+      <Text>Confirm Password</Text>
+      <TextInput
+        value={confirmPassword}
+        onChangeText={text => setConfirmPassword(text)}
+        secureTextEntry={true}
+        style={{ borderBottomWidth: 1, padding: 10 }}
+      />
+      {error && <Text>{error}</Text>}
+      <TouchableOpacity onPress={handleRegister}>
+        <Text>Register</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default RegisterScreen;
-
-const styles = StyleSheet.create({});
